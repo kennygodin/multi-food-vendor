6776da47e5a82813e26137a4ac39f89b9e164153
kennycodin <kenechukwuokoh30@gmail.com>
2024-02-16 01:21:26 +0100

    Fixed orderId type

--- src/app/api/orders/[id]/route.ts
+++ src/app/api/orders/[id]/route.ts
@@ -5,30 +5,27 @@ import { NextResponse } from 'next/server';
 export async function PUT(
   req: Request,
   { params }: { params: { id: string } }
-) {
-  const currentUser = await getCurrentUser();
-  if (!currentUser) {
-    return null;
-  }
-  if (currentUser && currentUser.role !== 'VENDOR') {
-    return NextResponse.error();
-  }
-
-  const id = params.id;
-  const body = await req.json();
-  const { status } = body;
-
+): Promise<void | Response | null> {
   try {
+    const currentUser = await getCurrentUser();
+
+    if (!currentUser || currentUser.role !== 'VENDOR') {
+      return NextResponse.error();
+    }
+
+    const id = params.id;
+    const body = await req.json();
+    const { status } = body;
+
+    // Update the order status
     const updatedOrder = await prisma.order.update({
-      where: {
-        id,
-      },
-      data: {
-        status,
-      },
+      where: { id },
+      data: { status },
     });
+
     return NextResponse.json(updatedOrder);
   } catch (error) {
+    console.error(error);
     return NextResponse.error();
   }
 }
